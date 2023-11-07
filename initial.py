ece68c3264aa96b67ef6d6ee918687c16b8c68a9
nexer nul <redpenguin4luca26@gmail.com>
2023-11-07 17:42:29 +0000

    state true/false

---
+++ initial.py
@@ -0,0 +1,43 @@
+#import copy
+# Define a function that takes in a state as a dictionary and returns True if the state meets the conditions and False if it does not
+def isValid(state):
+    if state["wolf"] is not state["person"] and state["wolf"] == state["goat"]:
+        return False
+    elif state["goat"] is not state["person"] and state["goat"] == state["cabbage"]:
+        return False
+    else:
+        return True
+
+# Define a function that takes in a state as a dictionary and returns a list of all valid states that can be reached from 1 move of the input state
+# This function will need to call the function isValid(state)
+def get_next_states(state):
+
+
+# Define a recursive function that takes in a current_state and win_state and returns the path to those states using the Depth First Search algorithm
+# This function will need to call the function get_next_states(state), as well as itself
+def dfs(current_state, win_state):
+    
+    
+# Test your code! Does it solve the river crossing riddle?
+initial_state = {
+    "wolf": False,
+    "goat": False,
+    "cabbage": False,
+    "person": False
+}
+
+win_state = {
+    "wolf": True,
+    "goat": True,
+    "cabbage": True,
+    "person": True
+}
+
+visited_states = [initial_state]
+path = []
+
+if dfs(initial_state, win_state):
+    for index, step in enumerate(path):
+        print("After move", index+1, "the state is ", step)
+else:
+    print("No solution found.")
